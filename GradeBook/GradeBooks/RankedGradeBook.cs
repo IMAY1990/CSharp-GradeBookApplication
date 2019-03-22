2e1ce7d64b570ab235572e08c37a59af1e5e8a33
IMAY1990 <christelkoenders@hotmail.com>
2019-03-22 16:39:06 +0100

    Update RankedGradeBook.cs
    En nu ook de tweede helft van de opdracht uitgevoerd

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -41,11 +41,17 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-            else
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
             {
-                base.CalculateStatistics();
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
-            
+            base.CalculateStudentStatistics(name);
         }
     }
 }
